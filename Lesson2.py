6a4da52c96b93e9449c96134932fb8cff7589259
W1sCont <tmrostik@gmail.com>
2025-08-24 20:28:01 +0200

    Lesson 2

---
+++ Lesson2.py
@@ -0,0 +1,58 @@
+    # змінні
+# name = input("Enter your name: ")
+# print("Hello, " + name)
+    # Data type
+string = "text 123" #str
+integer = 123 # int
+float = 15.4
+boolean = True # bool (True, False)
+none_type = None # None (empty box)
+
+    # Main operacion
+# print(10 + 10)
+# print(integer + 23) # + - / *
+# print(2 ** 3) # 2 в 3 степені
+# print(10 // 3) # повертає ціле чисто при ділення
+# print(25 % 10) # остача від ділення
+
+    # над строками
+# print("10" + "10")
+#
+# text = "ABCDE"
+# result = text + "FGH"
+# print(result)
+#
+# print("Hi" * 5)
+
+# n = 1
+# print(n + 1)
+# print(n + 1)
+# print(n + 1)
+# print(n)
+# n += 1
+# print(n)
+#
+# text = "Hi"
+# text += "Hi"
+# text += "Hi"
+# text += "Hi"
+#
+# print(text)
+
+# num1 = 47
+# num2 = 51
+#
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+#
+# num3 = 3
+# num4 = 6
+# num5 = 7
+#
+# result = (num3 + num4 + num5)/3
+# print(result)
+
+# num1 = int(input("Enter a number: "))
+# print(num1 -1, num1 +1)
