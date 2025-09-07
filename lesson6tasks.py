e680e4f65a0c5bbd1e4ba11c5c6501a2fe91f2f9
W1sCont <tmrostik@gmail.com>
2025-09-07 23:27:49 +0200

    task_12

---
+++ lesson6tasks.py
@@ -0,0 +1,37 @@
+#1 ++
+# num1 = int(input("Enter number 1: "))
+# num2 = int(input("Enter number 2: "))
+
+# for char in range(num1, num2 + 1):
+#     if char % 3 == 0 and char % 5 != 0:
+#         print(char)
+
+# char = num1
+# while char < num2:
+#     char += 1
+#     if char % 3 == 0 and char % 5 != 0:
+#         print(char)
+
+
+
+#2 ++
+# number = int(input("Enter a number: "))
+# result = 1
+# for char in range(1, number + 1):
+#     result *= char
+# print(result)
+
+
+
+
+# 3
+# name = input("Enter your name: ")
+# counter = 1
+# while len(name) > 0:
+#     print(name * counter)
+#     counter += 1
+#     if len(name) > 100:
+#         break
+
+
+#4 
