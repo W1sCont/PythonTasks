3bba762c9abfc057cb0c358794e348bee9bb2119
W1sCont <tmrostik@gmail.com>
2025-09-11 11:51:31 +0200

    110925

--- quess_the_number.py
+++ quess_the_number.py
@@ -13,36 +13,49 @@ import time
 # 2 якщо користувач замість числа вводить 0 програма закривається
 
 
-computer_number = random.randint(1, 10)  # випадкове число від 1 до 100
+computer_number = random.randint(1, 100)  # випадкове число від 1 до 100
 attempts = 0
 
 start_time = time.time()
 
 
+
 while True:
-    user_number = int(input("Enter a number: "))
+    user_number = int(input("Введіть число дів 1 до 100: "))
+
     if user_number == 0:
         break
     else:
         attempts += 1
+        print(computer_number) #
 
-    if computer_number > user_number:
-        print("My number >")
-    elif computer_number < user_number:
-        print("My number <")
-    else:
+    if computer_number != user_number:
+        print(f"" if 1 > abs(computer_number - user_number) < 10 else "Гаряче" if 11 > abs(computer_number - user_number) < 20 else "Тепло" if 21 > abs(computer_number - user_number) < 99 else "Холодно"   )
+    else: 
         end_time = time.time()
         result_time = end_time - start_time
 
         print(f"Grats {computer_number}. Your attempt {attempts}")
         print(f"result rime {round(result_time, 2)} sec")
-        yn = input("Continue enter 'y', for break enter 'n' " )
+        yes_no = input("Продовжити введіть y, для завершення введіть n: ")
+        # break
         
-        if yn == "y":
-            continue
-        elif yn == "n":
+        if yes_no == "n":
             break
+        elif yes_no == "y":
+            computer_number = random.randint(1, 100)
+            continue
+        # else:
+        #     while True:
+        #         yes_no = input("Продовжити введіть y, для завершення введіть n: ")
+            
+        #         if yes_no in "yn":
+        #             break
+
+
+            
         
+
             
             
             
